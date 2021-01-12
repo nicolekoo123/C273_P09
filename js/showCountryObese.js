3ffa43807cac59427d20790e78674da846aec23b
19047433 <19047433@myrp.edu.sg>
2021-01-12 12:25:52 +0800

    Second Commit

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+    $("#idCountry").change(function () {
+        var idCountry = $("#idCountry").val();
+        $.ajax({
+            type: "GET",
+            url: "https://cloudninep09.azurewebsites.net/getCountryDetails.php",
+            cache: false,
+            dataType: "JSON",
+            data: "id=" + idCountry,
+            success: function (response) {
+                var message = "<thead><tr><th>Population</th><th>Obese</th></tr></thead>";
+                message += "<tbody>";
+                message += "<tr><td>" + response[0].population + "</td>" +
+                        "<td>" + response[0].obese + "</td></tr>";
+                message += "</tbody>"
+                $("#obeseTable").html(message);
+            },
+            error: function (obj, textStatus, errorThrown) {
+                console.log("Error " + textStatus + ": " + errorThrown);
+            }
+        });
+    });
 
-    
 });
--- showCountryObese.php
+++ showCountryObese.php
@@ -19,31 +19,6 @@ mysqli_close($link);
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/showCountryObese.js" type="text/javascript"></script>
-        <script>
-            $(document).ready(function(){
-               $("#idCountry").change(function(){
-                  var idCountry = $("#idCountry").val();
-                  $.ajax({
-                     type: "GET",
-                     url: "http://localhost/C273/P09/C273_P09/getCountryDetails.php",
-                     cache: false,
-                     dataType: "JSON",
-                     data: "id=" + idCountry,
-                     success: function(response) {
-                         var message = "<thead><tr><th>Population</th><th>Obese</th></tr></thead>";
-                         message += "<tbody>";
-                         message +="<tr><td>" + response[0].population + "</td>" + 
-                                 "<td>" + response[0].obese + "</td></tr>";
-                         message += "</tbody>"
-                         $("#obeseTable").html(message);
-                     },
-                     error: function (obj, textStatus, errorThrown) {
-                            console.log("Error " + textStatus + ": " + errorThrown);
-                    }
-                  });
-               }); 
-            });
-        </script>
     </head>
 
     <body>
